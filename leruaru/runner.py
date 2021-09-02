6a5e7e5b3e263bc31418681513b5125e36307f1e
pavel <makovpavel@rambler.ru>
2021-09-02 20:02:59 +0300

    start

---
+++ leruaru/runner.py
@@ -0,0 +1,17 @@
+from scrapy.settings import Settings
+from scrapy.crawler import CrawlerProcess
+
+from leruaru import settings
+from leruaru.spiders.lerua import LeruaSpider
+
+
+
+if __name__ == '__main__':
+    crauler_settings = Settings()
+    crauler_settings.setmodule(settings)
+
+    process = CrawlerProcess(crauler_settings)
+    process.crawl(LeruaSpider)
+
+
+    process.start()
---
+++ leruaru/spiders/lerua.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class LeruaSpider(scrapy.Spider):
+    name = 'lerua'
+    allowed_domains = ['leroymerlin.ru']
+    start_urls = ['http://leroymerlin.ru/']
+
+    def parse(self, response):
+        print()
+        pass
